32ce97cc1ab8f21d0c3500154c01d44ffa4af0b7
jslisz-code <jslisz.code@outlook.com>
2026-08-26 14:54:43 -0500

    backgound animation edits

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
 function moveBackground(event){
   const shapes = document.querySelectorAll(".shape");
-  const x = event.clientX;
-  const y = event.clientY;
-  console.log(x, y)
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  
+  for (let i = 0; i < shapes.length; ++i) {
+    shapes[i].style.transform = 'translate(${x}px, ${y}px)'
+  }
 }
 
 function toggleContrast() {
